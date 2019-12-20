242c363d4e973c7ddcc8a2131a09f9e32cf45c72
Taras Shypytiak <taras.shypytiak@envisiontec.de>
2019-12-20 16:13:11 +0200

    idea.

--- GradientDescent.hpp
+++ GradientDescent.hpp
@@ -6,19 +6,22 @@ namespace tpr {
 	class GradientDescent {
 	public: // == TYPES ==
 		using ValueType = ValueT;
-		using VectorContainerT = std::array<ValueType, VarSize>;
+		//using VectorContainerT = std::array<ValueType, VarSize>;
+		using VectorT = typename F::VectorT;
 	public: // == CONSTANTS == 
 		static constexpr ValueT Epsilon = 1e-5;
 		static constexpr ValueT Lambda = 1.0f;
 		static constexpr size_t MaxIterations = 100'000;
 	public:
 		static void calculate(F f, GradientF gradF, const GradientContainerT& x0) {
-			VectorContainerT oldVec;
-			VectorContainerT currentVec = x0;
+			size_t N = F::NumVariables;// take num of vars from F
+
+			VectorT oldVec;
+			VectorT currentVec = x0;
 
 			for (size_t it = 0; it < MaxIterations; it++) {
 				oldVec = currentVec;
-				VectorContainerT gradientVec = gradF.get();
+				VectorT gradientVec = gradF.get();
 
 				for( size_t j = 0; j < VarSize; j++ )
 					currentVec[ j ] = currentVec[j] - Lambda * 

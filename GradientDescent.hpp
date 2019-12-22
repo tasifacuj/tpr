f2fcbfdd30abacd8675b7742974c8986be2b15ad
Taras Shypytiak <taras.shypytiak@artcom-venture.de>
2019-12-22 21:45:56 +0200

    gradient descent.

--- GradientDescent.hpp
+++ GradientDescent.hpp
@@ -1,31 +1,62 @@
 #pragma once
-#include <array>
+#include <cmath>
+#include <cassert>
 
 namespace tpr {
-	template<typename ValueT, int VarSize, typename F, typename GradientF>
-	class GradientDescent {
+	template< typename F,
+		typename IndexType = size_t
+	>
+	class StepSplitGradientDescent {
 	public: // == TYPES ==
-		using ValueType = ValueT;
-		//using VectorContainerT = std::array<ValueType, VarSize>;
-		using VectorT = typename F::VectorT;
+		using ValueType = typename F::ValueType;
+		using VectorT	= typename F::VectorT;
 	public: // == CONSTANTS == 
-		static constexpr ValueT Epsilon = 1e-5;
-		static constexpr ValueT Lambda = 1.0f;
-		static constexpr size_t MaxIterations = 100'000;
+		static constexpr ValueType	Epsilon			= 1e-5f;
+		static constexpr IndexType	MaxIterations	= 100'000;
+		static constexpr ValueType	SplitEps		= 0.1f;
+		static constexpr ValueType  SplitDelta		= 0.95f;
+		static constexpr ValueType  Lambda			= 1.0f;
 	public:
-		static void calculate(F f, GradientF gradF, const GradientContainerT& x0) {
-			size_t N = F::NumVariables;// take num of vars from F
+		static VectorT calculate( const VectorT& x0, ValueType lambda, IndexType& it) {
+			IndexType N = F::N;// take num of vars from F
+			VectorT oldXVec;
+			VectorT currentXVec = x0;
+			// || grad( f( x[k] ) )||^2
+			ValueType squaredNorm = 0.0f;
 
-			VectorT oldVec;
-			VectorT currentVec = x0;
+			for (it = 0; it < MaxIterations; it++) {
+				// save old value
+				oldXVec = currentXVec;
+				// evaluate gradient
+				VectorT gradientVec = F::gradient(currentXVec);
 
-			for (size_t it = 0; it < MaxIterations; it++) {
-				oldVec = currentVec;
-				VectorT gradientVec = gradF.get();
+				// evaluate new value
+				for (IndexType j = 0; j < N; j++)
+					currentXVec[j] = currentXVec[j] - lambda * gradientVec[j];
 
-				for( size_t j = 0; j < VarSize; j++ )
-					currentVec[ j ] = currentVec[j] - Lambda * 
-			}
+				// evaluate square of gradient norm
+				squaredNorm = 0.0f;
+				for (IndexType idx = 0; idx < N; idx++)
+					squaredNorm += gradientVec[idx] * gradientVec[idx];
+
+				// select lambda from next condition:
+				// f( x[k+1] ) = f( x[k] ) - lambda[k] * grad(f[ x[ k ] ]) <= f(x[k]) - eps * lambda[k] * || grad( f( x[k] ) )|| ^ 2
+				while (F::apply(currentXVec) > F::apply(oldXVec) - SplitEps * lambda * squaredNorm) {
+					lambda = SplitDelta * lambda;
+					currentXVec = oldXVec;
+
+					for (IndexType j = 0; j < N; j++)
+						currentXVec[j] = currentXVec[j] - lambda * gradientVec[j];
+				}
+
+				ValueType diff = std::fabs(F::apply(currentXVec) - F::apply(oldXVec));
+
+				if (diff < Epsilon)
+					return currentXVec;
+			}// for
+
+			assert(0 && "Failed");
+			return currentXVec;
 		}
 	};
-}
+}// namespace tpr
--- main.cpp
+++ main.cpp
@@ -1,5 +1,55 @@
 #include "GradientDescent.hpp"
 
+#include <iostream>
+#include <array>
+
+/**
+  * f(x) = 10 * x1^2 + x2 ^ 2
+  * df
+  * --- = 20 * x1
+  * dx1
+  * df/dx2 = 2*x2
+  */
+template<typename ValueT = float>
+class QuadF {
+public:// == CONSTANTS ==
+	static constexpr size_t N = 2;
+public: // == TYPES ==
+	using ValueType = ValueT;
+	using VectorT = std::array<ValueType, N>;
+public: // == METHODS ==
+	static ValueType apply( const VectorT& args ) {
+		return 10 * args[0] * args[0] + args[1] * args[1];
+	}
+
+	static VectorT gradient( const VectorT& args ) {
+		VectorT tmp;
+		tmp[ 0 ] = 20 * args[0];
+		tmp[ 1 ] = 2 * args[ 1 ];
+		return tmp;
+	}
+};
+
 int main() {
+	using F = QuadF<>;
+	using GradientDescent = tpr::StepSplitGradientDescent<F>;
+	using Vec = typename F::VectorT;
+	Vec x0{ 10.0f, 10.0f };
+	size_t it = 0;
+	Vec result = GradientDescent::calculate(x0, GradientDescent::Lambda, it);
+	/*std::cerr << "f = 10 * x1^2 + x2 ^ 2, min(f) = {";
 	
+	for (size_t idx = 0; idx < F::N - 1; idx++) {
+		std::cerr << result[idx] << ", ";
+	}
+
+	std::cerr << result[F::N - 1] << "}\n";*/
+
+	std::cerr << "Value: " << F::apply( result ) << std::endl;
+	std::cerr << "Point: ";
+	
+	for (size_t i = 0; i < result.size(); i++)
+		std::cerr << result[i] << ' ';
+	std::cerr << std::endl << "Number of iterations:" << it << std::endl;
+	return 0;
 }

c87e49157c6579d33929631281d87b9383669045
Taras Shypytiak <taras.shypytiak@envisiontec.de>
2019-12-23 15:41:32 +0200

    First run asserted.

--- PenaltyFunction.hpp
+++ PenaltyFunction.hpp
@@ -7,7 +7,18 @@
 namespace tpr {
 	/**
 	 * @brief penalty function algorithm
-	 * F(x, r[k]) = f(x) + r[k] * alpha(x) -> min
+	 * while true
+	 * do
+	 *     F(x, r[k]) = f(x_opt) + r[k] * alpha(x_opt)
+	 *	   x_opt = min( F(x, r[k]) )
+	 *	   
+	 *     if r[k] * alpha(x_opt) <= Epsilon
+	 *     then
+	 *        return x_opt
+	 *	   else
+	 *         r[k+1] = Beta * r[k]
+	 *     end
+	 * done
 	 */
 	template<
 		typename FT, //minimizing function
@@ -27,13 +38,33 @@ namespace tpr {
 			using Tail = R1< ValueT, VecT, PParam, GiFuncTypes ... >;
 
 			static ValueT apply(const VecT& xArgs) {
-				return std::pow(std::max(0, -1 * G::apply(xArgs)), PParam) + Tail::apply(xArgs);
+				return std::pow(std::max(0.0, double(-1.0 * G::apply(xArgs))), PParam) + Tail::apply(xArgs);
 			}
 
 			static VecT gradient(const VecT& xArgs) {
-				ValueT v = std::pow(std::max(0, -1 * G::apply(xArgs)), PParam);
-				VecT g = v > 0 ? std::pow(G::gradient(xArgs), PParam) : VecT();
-				return g + Tail::gradient( xArgs );
+				ValueT v = std::pow(std::max(0.0, -1.0 * G::apply(xArgs)), PParam);
+
+				if (v > 0) {
+					/**
+					 * f(x) = (x1 + x2 - 7 )^3
+					 * dfdx1 = 3*(x1+x2 - 7)^2 * (x1+x2 - 7)' = 3*(x1+x2 - 7)^2 * 1
+					 */
+					VecT g = G::gradient(xArgs);
+					
+					for (IndexType idx = 0; idx < g.size(); idx++) {
+						g[idx] = PParam * std::pow(-1 * G::apply(xArgs), PParam - 1) * std::pow(g[idx], PParam - 1);
+					}
+
+					VecT tailGrad = Tail::gradient(xArgs);
+
+					for (IndexType idx = 0; idx < g.size(); idx++) {
+						g[idx] += tailGrad[idx];
+					}
+					
+					return g;
+				}
+				
+				return VecT();
 			}
 		};
 
@@ -41,18 +72,37 @@ namespace tpr {
 		template<typename ValueT, typename VecT, int PParam, typename G>
 		struct R1<ValueT, VecT, PParam, G> {
 			static ValueT apply(const VecT& xArgs) {
-				return std::pow(std::max(0, -1 * G::apply(xArgs)), PParam );
+				return std::pow(std::max(0.0, -1.0 * G::apply(xArgs)), PParam );
 			}
 
 			static VecT gradient(const VecT& xArgs) {
-				ValueT v = std::pow(std::max(0, -1 * G::apply(xArgs)), PParam);
-				VecT g = v > 0 ? std::pow(G::gradient(xArgs), PParam) : VecT();
-				return g;
+				ValueT v = std::pow(std::max(0.0, -1.0 * G::apply(xArgs)), PParam);
+
+				if (v > 0) {
+					/**
+					 * f(x) = (x1 + x2 - 7 )^3
+					 * dfdx1 = 3*(x1+x2 - 7)^2 * (x1+x2 - 7)' = 3*(x1+x2 - 7)^2 * 1
+					 */
+					VecT g = G::gradient(xArgs);
+
+					for (IndexType idx = 0; idx < g.size(); idx++) {
+						g[idx] = PParam * std::pow(-1 * G::apply(xArgs), PParam - 1) * std::pow(g[idx], PParam - 1);
+					}
+
+					return g;
+				}
+
+				return VecT();
 			}
 		};
 
+		static constexpr int		PCoef = 2;
+
 		using R1Sum = R1<ValueType, VectorT, ThisT::PCoef, GiFuncTypes ...>;
 
+		/**
+		 * alpha(x) = sum( R1( gi(x) ) ) + sum( R2( hi(x) ) )
+		 */
 		template<typename ValueT, typename VecT, typename ROneSum, typename RTwo = void>
 		struct AlphaFunc {
 			static ValueT apply(const VecT& xArgs) {
@@ -68,9 +118,8 @@ namespace tpr {
 
 	public: // == CONSTANTS ==
 		static constexpr ValueType	Beta	= 2;	//!< growth factor.
-		static constexpr ValueType	Epsilon	= 1e-5f;//!< accuracy
+		static constexpr ValueType	Epsilon	= 1e-4f;//!< accuracy
 		static constexpr ValueType	Rk		= 0.5f; //!< positive constant
-		static constexpr int		PCoef		= 2;
 		static constexpr IndexType	N		= TargetF::N;
 		static constexpr IndexType	MaxPIterations = 100'000;
 
@@ -78,34 +127,52 @@ namespace tpr {
 
 	public: // == TYPES ==
 
-		///**
-		// * Penalty function routine
-		// */
+		/**
+		 * Penalty function routine
+		 * F(x, r[k]) = f(x) + r[k] * alpha(x)
+		 */
 		template<
 			typename ValueT,
 			typename VecT,
 			typename F,
 			typename A
 		>
-		struct PFFunc {
+		struct FxRkFunction {
+
+			using ValueType = ValueT;
+			using VectorT	= VecT;
+			static constexpr int N = ThisT::N;
+
 			static ValueT apply(const VecT& xArgs) {
 				return F::apply(xArgs) + ThisT::sRk * A::apply( xArgs );
 			}
 
 			static VecT gradient(const VecT& xArgs) {
-				return F::gradient(xArgs) + ThisT::sRk * A::gradient(xArgs);
+				VecT alphaGrad = A::gradient(xArgs);
+
+				for( IndexType idx = 0; idx < alphaGrad.size(); idx++)
+					alphaGrad[ idx ] *= ThisT::sRk;
+				
+				VecT fGrad = F::gradient(xArgs);
+				assert(fGrad.size() == alphaGrad.size());
+				VecT rval;
+				
+				for (size_t idx = 0; idx < fGrad.size(); idx++)
+					rval[idx] = fGrad[idx] + alphaGrad[idx];
+				
+				return rval;
 			}
 		};
 		
 
 	public: // == METHODS ==
 		static VectorT evaluate(const VectorT& x0) {
-			ThisT::rk = Rk;
+			ThisT::sRk = Rk;
 			VectorT xArgs = x0;
 			// prepare new penalty function
-			using PFRoutine = PFFunc<ValueType, VectorT, TargetF, Alpha>;
+			using FxRk = FxRkFunction<ValueType, VectorT, TargetF, Alpha>;
 
-			using GradientDescent = StepSplitGradientDescent<PFRoutine>;
+			using GradientDescent = StepSplitGradientDescent<FxRk>;
 			IndexType idx = 0;
 
 			for (; idx < MaxPIterations; idx++ ) {
@@ -114,7 +181,7 @@ namespace tpr {
 				// find min( F(x, rk) )
 				VectorT xOptLoc = GradientDescent::calculate(xArgs, GradientDescent::Lambda, it);
 
-				if (ThisT::sRk * PF::apply(xOptLoc) <= Epsilon) {
+				if (ThisT::sRk * Alpha::apply(xOptLoc) <= Epsilon) {
 					return xOptLoc;
 				}else {
 					// r[k+1] = r[k] * B
@@ -136,5 +203,5 @@ namespace tpr {
 		typename IndexType,
 		typename ... GiFuncTypes
 	>
-	PenaltyFunction<FT, IndexType, GiFuncTypes ...>::ValueType PenaltyFunction<FT, IndexType, GiFuncTypes ...>::sRk = 0.0f;
+	typename PenaltyFunction<FT, IndexType, GiFuncTypes ...>::ValueType PenaltyFunction<FT, IndexType, GiFuncTypes ...>::sRk = 0.0f;
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-#include "GradientDescent.hpp"
-
 #include <iostream>
 #include <array>
+
+#include "GradientDescent.hpp"
+#include "PenaltyFunction.hpp"
 
 /**
   * f(x) = 10 * x1^2 + x2 ^ 2
@@ -31,26 +32,134 @@ public: // == METHODS ==
 	}
 };
 
+/**
+ * f(x) = ( x1 - 6 )^2 + ( x2 - 7 )^2
+ * dfdx1 = 2( x1 - 6 )
+ * dfdx2 = 2( x2 - 7 )
+ */
+struct Fx {
+	static constexpr size_t N = 2;
+	using ValueType = double;
+	using VectorT = std::array<ValueType, N>;
+
+	static ValueType apply(const VectorT& args) {
+		return (args[0] - 6) * (args[0] - 6) + (args[1] - 7)*(args[1] - 7);
+	}
+
+	static VectorT gradient(const VectorT& args) {
+		// 20 * x1 + 2 * x2
+		VectorT tmp;
+		tmp[0] = 2 * (args[0] - 6);
+		tmp[1] = 2 * (args[1] - 7);
+		return tmp;
+	}
+};
+
+/**
+ * g1(x) = 3x1 + 2x2 - 6 >= 0
+ * dfdx1 = 3
+ * dfdx2 = 2
+ */
+struct G1 {
+	static constexpr size_t N = 2;
+	using ValueType = double;
+	using VectorT = std::array<ValueType, N>;
+
+	static ValueType apply(const VectorT& args) {
+		return 3 * args[0] + 2 * args[1] - 6;
+	}
+
+	static VectorT gradient(const VectorT&) {
+		VectorT tmp;
+		tmp[0] = 3;
+		tmp[1] = 2;
+		return tmp;
+	}
+};
+
+/**
+ * g2(x) = x1 - x2 + 3 >= 0
+ * dfdx1 = 1
+ * dfdx2 = -1
+ */
+struct G2 {
+	static constexpr size_t N = 2;
+	using ValueType = double;
+	using VectorT = std::array<ValueType, N>;
+
+	static ValueType apply(const VectorT& args) {
+		return  args[0] - args[1] + 3;
+	}
+
+	static VectorT gradient(const VectorT&) {
+		VectorT tmp;
+		tmp[0] = 1;
+		tmp[1] = -1;
+		return tmp;
+	}
+};
+
+/**
+ * g3(x) = -x1 - x2 + 7 >= 0
+ * dfdx1 = -1
+ * dfdx2 = -1
+ */
+struct G3 {
+	static constexpr size_t N = 2;
+	using ValueType = double;
+	using VectorT = std::array<ValueType, N>;
+
+	static ValueType apply(const VectorT& args) {
+		return  -1 * args[0] - args[1] + 7;
+	}
+
+	static VectorT gradient(const VectorT&) {
+		VectorT tmp;
+		tmp[0] = -1;
+		tmp[1] = -1;
+		return tmp;
+	}
+};
+
+/**
+ * g4(x) = -(2/3)x1 + x2 + 4/3 >= 0
+ * dfdx1 = -2/3
+ * dfdx2 = 1
+ */
+struct G4 {
+	static constexpr size_t N = 2;
+	using ValueType = double;
+	using VectorT = std::array<ValueType, N>;
+
+	static ValueType apply(const VectorT& args) {
+		return  (-2.0f / 3.0f) * args[0] + args[1] + 4.0f/3.0f;
+	}
+
+	static VectorT gradient(const VectorT&) {
+		VectorT tmp;
+		tmp[0] = -2.0f / 3.0f;
+		tmp[1] = 1;
+		return tmp;
+	}
+};
+
 int main() {
-	using F = QuadF<>;
+	/*using F = QuadF<>;
 	using GradientDescent = tpr::StepSplitGradientDescent<F>;
 	using Vec = typename F::VectorT;
 	Vec x0{ 10.0f, 10.0f };
 	size_t it = 0;
 	Vec result = GradientDescent::calculate(x0, GradientDescent::Lambda, it);
-	/*std::cerr << "f = 10 * x1^2 + x2 ^ 2, min(f) = {";
-	
-	for (size_t idx = 0; idx < F::N - 1; idx++) {
-		std::cerr << result[idx] << ", ";
-	}
-
-	std::cerr << result[F::N - 1] << "}\n";*/
 
 	std::cerr << "Value: " << F::apply( result ) << std::endl;
 	std::cerr << "Point: ";
 	
 	for (size_t i = 0; i < result.size(); i++)
 		std::cerr << result[i] << ' ';
-	std::cerr << std::endl << "Number of iterations:" << it << std::endl;
+	std::cerr << std::endl << "Number of iterations:" << it << std::endl;*/
+
+	using PF = tpr::PenaltyFunction<Fx, size_t, G1, G2, G3, G4> ;
+	PF::VectorT x0{ 6.0f, 7.0f };
+	PF::VectorT xOpt = PF::evaluate(x0);
 	return 0;
 }

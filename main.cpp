29df7b9847353c7a43868ac35e0c53dc637bea3b
Taras Shypytiak <taras.shypytiak@envisiontec.de>
2019-12-24 19:43:08 +0200

    started subj_17 data.

--- main.cpp
+++ main.cpp
@@ -161,24 +161,79 @@ struct G4 {
 	}
 };
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// my task
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace subj_17 {
+	/**
+	 * f(x) = 3x0  + 3x1  + 9x2  + 9x3  + 5x4  + 5x5
+	 *      + 3x6  + 3x7  + 6x8  + 6x9 + 8x10 + 8x11
+	 *		+ 8x12 + 8x13 + 2x14 + 2x15 + 5x16 + 5x17
+	 */
+	struct Fx {
+		static constexpr size_t N = 18;
+		using ValueType = double;
+		using VectorT = std::array<ValueType, N>;
 
+		static ValueType apply(const VectorT& args) {
+			return 3.0 * args[0]  + 3.0 * args[1]  + 9.0 * args[2] + 9.0 * args[3] + 5.0 * args[4]  + 5.0 * args[5]
+				 + 3.0 * args[6]  + 3.0 * args[7]  + 6.0 * args[8] + 6.0 * args[9] + 8.0 * args[10] + 8.0 * args[11]
+				 + 8.0 * args[12] + 8.0 * args[13] + 2.0 * args[8] + 2.0 * args[9] + 5.0 * args[10] + 5.0 * args[11]
+				;
+		}
 
+		static VectorT gradient(const VectorT&) {
+			VectorT tmp;
+			// first row
+			tmp[0] = 3.0f ;
+			tmp[1] = 3.0f ;
+			tmp[2] = 9.0f ;
+			tmp[3] = 9.0f ;
+			tmp[4] = 5.0f ;
+			tmp[5] = 5.0f ;
+
+			//2-nd row
+			tmp[6] = 3.0f ;
+			tmp[7] = 3.0f;
+			tmp[8] = 6.0f;
+			tmp[9] = 6.0f;
+			tmp[10] = 8.0f;
+			tmp[11] = 8.0f;
+
+			//3-rd row
+			tmp[12] = 8.0f;
+			tmp[13] = 8.0f;
+			tmp[14] = 2.0f;
+			tmp[15] = 2.0f;
+			tmp[16] = 5.0f;
+			tmp[17] = 5.0f;
+			return tmp;
+		}
+	};
+
+	/**
+	 * g1(x) = 1.5x111 + 0.75x121 + 2.5*x131 + 1.282* sqrt( 0.083 * x111^2 + 0.0208*x121^2 + 0.083*x131^2 ) - 250 <= 0
+	 */
+	struct G1 {
+		static constexpr size_t N = 18;
+		using ValueType = double;
+		using VectorT = std::array<ValueType, N>;
+
+		static ValueType apply(const VectorT& args) {
+			return 1.5 * args[0] + 0.75 * args[2] + 2.5 * args[4] + 1.282* std::sqrt( 0.083 * args[0] * args[0] + 0.0208 * args[2] * args[2] + 0.083 * args[4] * args[ 4 ] ) - 250.0
+				;
+		}
+
+		static VectorT gradient(const VectorT& xargs) {
+			VectorT tmp;
+			tmp[0] = 1.5 + 1.283 * 0.5 * std::pow(0.083 * xargs[0] * xargs[0] + 0.0208 * xargs[2] * xargs[2] + 0.083 * xargs[4] * xargs[4], -0.5) * 2 * 0.083 * xargs[0];
+			tmp[1] = -2.0;
+			return tmp;
+		}
+	};
+}
 
 int main() {
-	/*using F = QuadF<>;
-	using GradientDescent = tpr::StepSplitGradientDescent<F>;
-	using Vec = typename F::VectorT;
-	Vec x0{ 10.0f, 10.0f };
-	size_t it = 0;
-	Vec result = GradientDescent::calculate(x0, GradientDescent::Lambda, it);
-
-	std::cerr << "Value: " << F::apply( result ) << std::endl;
-	std::cerr << "Point: ";
-	
-	for (size_t i = 0; i < result.size(); i++)
-		std::cerr << result[i] << ' ';
-	std::cerr << std::endl << "Number of iterations:" << it << std::endl;*/
-
 	using PF = tpr::PenaltyFunction<Fx, size_t, G1, G2, G3, G4> ;
 	PF::VectorT x0{ 6.0f, 7.0f };
 	PF::VectorT xOpt = PF::evaluate(x0);

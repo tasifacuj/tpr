6b8e02dc3ea79af57abe9add3106d8e8f88a919c
Taras Shypytiak <taras.shypytiak@artcom-venture.de>
2019-12-26 23:29:20 +0200

    debugga simplified.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include "PenaltyFunction.hpp"
 #include "subj_17.hpp"
 #include "TrainingModel.hpp"
-
+#include "subj_17_simplified.hpp"
 
 ///**
 //  * f(x) = 10 * x1^2 + x2 ^ 2
@@ -43,8 +43,7 @@
 // my task
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int main() {
-#if 1
+static void test_subj_17(){
 	assert(tpr::subj_17::model_index_to_index.size() == 18);
 	using PF = tpr::PenaltyFunction<tpr::subj_17::Fx, size_t, tpr::subj_17::G1, tpr::subj_17::G2, tpr::subj_17::G3, tpr::subj_17::G4, tpr::subj_17::G5, tpr::subj_17::G6, tpr::subj_17::G7, tpr::subj_17::G8, tpr::subj_17::G9>;
 	PF::VectorT x0;
@@ -60,7 +59,7 @@ int main() {
 		out << "x[ " << modelIndex << " ]opt = " << std::round(xOpt[idx]) << " --> " << tpr::subj_17::model_index_to_description_conv[modelIndex] << '\n';
 	}
 
-	PF::ValueType sumProdA = xOpt[tpr::subj_17::model_index_to_index[111]] 
+	PF::ValueType sumProdA = xOpt[tpr::subj_17::model_index_to_index[111]]
 		+ xOpt[tpr::subj_17::model_index_to_index[112]]
 		+ xOpt[tpr::subj_17::model_index_to_index[211]]
 		+ xOpt[tpr::subj_17::model_index_to_index[212]]
@@ -86,10 +85,30 @@ int main() {
 	out << "sum(B) = " << sumProdB << std::endl;
 	out << "sum(C) = " << sumProdC << std::endl;
 	out.flush();
-#else
+}
+
+static void test_doc_example() {
 	using TrainPF = tpr::PenaltyFunction<tpr::TrainingModel::Fx, size_t, tpr::TrainingModel::G1, tpr::TrainingModel::G2, tpr::TrainingModel::G3, tpr::TrainingModel::G4>;
 	TrainPF::VectorT x0T{ 6.0f, 7.0f };
 	TrainPF::VectorT xOptT = TrainPF::evaluate(x0T);
-#endif
+}
+
+static void test_subj_17_simplified() {
+	using PF = tpr::PenaltyFunction<tpr::simplified::Fx, size_t, tpr::simplified::G1, tpr::simplified::G2, tpr::simplified::G3, tpr::simplified::G4>;
+	PF::VectorT x0;
+
+	for (size_t idx = 0; idx < x0.size(); idx++)
+		x0[idx] = 50;
+
+	PF::VectorT xOpt = PF::evaluate(x0);
+	
+
+	for (size_t idx = 0; idx < PF::N; idx++) {
+		std::cerr << "x[ " << idx << " ]opt = " << std::round(xOpt[idx]) << '\n';
+	}	
+}
+
+int main() {
+	test_subj_17_simplified();
 	return 0;
 }
---
+++ subj_17_simplified.hpp
@@ -0,0 +1,131 @@
+#pragma once
+#include <array>
+namespace tpr {
+	namespace simplified {
+		template<typename T>
+		T sqr(T val) {
+			return val * val;
+		}
+
+		struct Fx {
+			static constexpr size_t N = 3;
+			using ValueType = double;
+			using VectorT = std::array<ValueType, N>;
+
+			static ValueType apply(const VectorT& args) {
+				return 3 * args[ 0 ] + 9 * args[ 1 ] + 5 * args[ 2 ];
+			}
+
+			static VectorT gradient(const VectorT&) {
+				return { 3, 9, 5 };
+			}
+		};
+
+		struct G1 {
+			static constexpr size_t N = 3;
+			using ValueType = double;
+			using VectorT = std::array<ValueType, N>;
+
+			// g1(x) = 1.5x111 + 0.75x121 + 2.5*x131 + 1.282* sqrt( 0.083 * x111^2 + 0.0208*x121^2 + 0.083*x131^2 ) - 250 <= 0
+			static ValueType apply(const VectorT& args) {
+				auto fff = 1.5 * args[0] + 0.75 * args[1] + 2.5 * args[2]
+					+ 1.282 * std::sqrt(0.083 * sqr(args[0])
+						+ 0.0208 * sqr(args[1])
+						+ 0.083 * sqr(args[2])
+					) - 250;
+
+				return 1.5 * args[ 0 ] + 0.75 * args[ 1 ] + 2.5 * args[ 2 ]
+					+ 1.282 * std::sqrt(0.083 * sqr(args[0])
+						+ 0.0208 * sqr(args[ 1 ])
+						+ 0.083 * sqr(args[ 2 ])
+					) - 250
+					;
+			}
+
+			/**
+			 * g1(x) = 1.5x111 + 0.75x121 + 2.5*x131 + 1.282* sqrt( 0.083 * x111^2 + 0.0208*x121^2 + 0.083*x131^2 ) - 250 <= 0
+			 * v[0]: dg1/dx111 = (1.5 + 1.282 * 0.5 * ( 0.083x111^2 + 0.0208x121^2 + 0.083x131^2 )^(-0.5)) * 2 * 0.083x111
+			 * v[2]: dg1/dx121 = (0.75 + 1.282 * 0.5 * ( 0.083x111^2 + 0.0208x121^2 + 0.083x131^2 )^(-0.5)) * 2 * 0.0208x121
+			 * v[4]: dg1/dx131 = (2.5 + 1.282 * 0.5 * ( 0.083x111^2 + 0.0208x121^2 + 0.083x131^2 )^(-0.5)) * 2 * 0.083x131
+			 */
+			static VectorT gradient(const VectorT& xargs) {
+				VectorT tmp;
+				memset(&tmp[0], 0, sizeof(ValueType) * N);
+				//x111
+				// v[0]: dg1/dx111 = (1.5 + 1.282 * 0.5 * ( 0.083x111^2 + 0.0208x121^2 + 0.083x131^2 )^(-0.5)) * 2 * 0.083x111
+				tmp[0] = (1.5 + 1.282 * 0.5
+					* std::pow(
+						(0.083 * sqr(xargs[0]) + 0.0208 * sqr(xargs[1]) + 0.083 * sqr(xargs[2])),
+						-0.5)
+					)
+					* 2 * 0.083 * xargs[0];
+				// 121
+				// v[2]: dg1/dx121 = (0.75 + 1.282 * 0.5 * ( 0.083x111^2 + 0.0208x121^2 + 0.083x131^2 )^(-0.5)) * 2 * 0.0208x121
+				tmp[1] = (0.75 + 1.282 * 0.5
+					* std::pow(
+						(0.083 * sqr(xargs[0]) + 0.0208 * sqr(xargs[1]) + 0.083 * sqr(xargs[2])),
+						-0.5)
+					)
+					* 2 * 0.0208 * xargs[1];
+
+				// 131
+				// v[4]: dg1 / dx131 = (2.5 + 1.282 * 0.5 * (0.083x111 ^ 2 + 0.0208x121 ^ 2 + 0.083x131 ^ 2) ^ (-0.5)) * 2 * 0.083x131
+				tmp[2] = (2.5 + 1.282 * 0.5
+					* std::pow(
+					(0.083 * sqr(xargs[0]) + 0.0208 * sqr(xargs[1]) + 0.083 * sqr(xargs[2])),
+						-0.5)
+					)
+					* 2 * 0.083 * xargs[2];
+				return tmp;
+			}
+		};
+
+		struct G2 {
+			static constexpr size_t N = 3;
+			using ValueType = double;
+			using VectorT = std::array<ValueType, N>;
+
+			// g1(x) = 1.5x111 + 0.75x121 + 2.5*x131 + 1.282* sqrt( 0.083 * x111^2 + 0.0208*x121^2 + 0.083*x131^2 ) - 250 <= 0
+			static ValueType apply(const VectorT& args) {
+				auto fff = 300 - args[0];
+				return 300 - args[0];
+			}
+
+			static VectorT gradient(const VectorT& ) {
+				return { -1.0, 0.0, 0.0 };
+			}
+		};
+
+		struct G3 {
+			static constexpr size_t N = 3;
+			using ValueType = double;
+			using VectorT = std::array<ValueType, N>;
+
+			// g1(x) = 1.5x111 + 0.75x121 + 2.5*x131 + 1.282* sqrt( 0.083 * x111^2 + 0.0208*x121^2 + 0.083*x131^2 ) - 250 <= 0
+			static ValueType apply(const VectorT& args) {
+				auto fff = 170 - args[1];
+				return 170 - args[1];
+			}
+
+			static VectorT gradient(const VectorT& ) {
+				return { 0.0, -1.0, 0.0 };
+			}
+		};
+
+		struct G4 {
+			static constexpr size_t N = 3;
+			using ValueType = double;
+			using VectorT = std::array<ValueType, N>;
+
+			// g1(x) = 1.5x111 + 0.75x121 + 2.5*x131 + 1.282* sqrt( 0.083 * x111^2 + 0.0208*x121^2 + 0.083*x131^2 ) - 250 <= 0
+			static ValueType apply(const VectorT& args) {
+				auto fff = 250 - args[2];
+				return 250 - args[2];
+			}
+
+			static VectorT gradient(const VectorT& ) {
+				return { 0.0, 0.0, -1.0 };
+			}
+		};
+	}
+}
